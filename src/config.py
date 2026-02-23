aa67a7ea069c68e73a654d5613a87af36d09e526
Jeremy <jilammon10@gmail.com>
2026-02-23 00:52:59 -0800

    More config data

--- src/config.py
+++ src/config.py
@@ -1,13 +1,16 @@
-from pydantic_settings import BaseSettings
-
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr
 
 class Config(BaseSettings):
     SQLALCHEMY_DATABASE_URL: str
 
-    APP_VERSION: str = "1.0"
-    APP_NAME: str = "Todo App"
+    # JWT
+    jwt_secret: SecretStr
+    jwt_algorithm: str = "HS256"
+    jwt_expire_minutes: int = 30
 
-    DEBUG: bool = False
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
 
 
 settings = Config()
