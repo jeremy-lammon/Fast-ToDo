d546cf020583825b36c85029d79c6109f2982604
Jeremy <jilammon10@gmail.com>
2026-02-22 17:05:28 -0800

    Updated user model

--- src/users/models.py
+++ src/users/models.py
@@ -1,10 +1,10 @@
-from sqlalchemy.orm import relationship, Mapped, mapped_column
-from database import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from src.database.base_class import Base
 
-class User(Base):
+
+class UserModel(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(unique=True, index=True)
-    hashed_password: Mapped[str] = mapped_column()
 
